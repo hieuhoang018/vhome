f82e96ca742b940457c654360225fe39cbd976e3
Hoang Vu Hieu <hieu.hoang@tuni.fi>
2025-05-21 17:59:53 +0300

    feat: implemented navbar

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,75 @@
+import { Heart, Search, ShoppingCart, User } from "lucide-react"
+import Image from "next/image"
+import Link from "next/link"
+import logo from "@/app/assets/logo.jpg"
+
+export default function MainHeader() {
+  return (
+    <>
+      <header className="fixed top-0 left-0 right-0 z-50 transition-all duration-300">
+        <div className="container mx-auto px-4 flex items-center justify-between">
+          {/* Logo */}
+          <Link href="/">
+            <Image src={logo} alt="VHome logo" width={80}/>
+          </Link>
+
+          {/* Navigation */}
+          <nav className="hidden md:flex items-center space-x-8">
+            <Link
+              href="#"
+              className="text-furniture-charcoal hover:text-furniture-navy font-medium transition-colors"
+            >
+              Home
+            </Link>
+            <Link
+              href="#categories"
+              className="text-furniture-charcoal hover:red font-medium transition-colors"
+            >
+              Shop
+            </Link>
+            <Link
+              href="#collections"
+              className="text-furniture-charcoal hover:text-furniture-navy font-medium transition-colors"
+            >
+              Collections
+            </Link>
+            <Link
+              href="#about"
+              className="text-furniture-charcoal hover:text-furniture-navy font-medium transition-colors"
+            >
+              About
+            </Link>
+          </nav>
+
+          {/* Action buttons */}
+          <div className="hidden md:flex items-center space-x-4">
+            <button
+              type="button"
+              className="p-2 rounded hover:bg-gray-100 transition-colors"
+            >
+              <Search className="h-5 w-5" />
+            </button>
+            <button
+              type="button"
+              className="p-2 rounded hover:bg-gray-100 transition-colors"
+            >
+              <User className="h-5 w-5" />
+            </button>
+            <button
+              type="button"
+              className="p-2 rounded hover:bg-gray-100 transition-colors"
+            >
+              <Heart className="h-5 w-5" />
+            </button>
+            <button
+              type="button"
+              className="p-2 rounded hover:bg-gray-100 transition-colors"
+            >
+              <ShoppingCart className="h-5 w-5 mr-2" />
+            </button>
+          </div>
+        </div>
+      </header>
+    </>
+  )
+}
