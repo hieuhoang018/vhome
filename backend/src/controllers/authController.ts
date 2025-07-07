e5acc46a890159fbbc21e3c4db55692385032852
Hoang Vu Hieu <hieu.hoang@tuni.fi>
2025-07-08 00:23:56 +0300

    fix: fix cookies errors relating to logging out

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -258,11 +258,17 @@ export const updatePassword = catchAsync(
 )
 
 export const logout = (req: Request, res: Response) => {
-  res.cookie("jwt", "", {
-    expires: new Date(Date.now() + 10 * 1000), // Expire the cookie shortly
+  const sameSiteOption: "lax" | "strict" | "none" =
+    process.env.NODE_ENV === "production" ? "none" : "lax"
+
+  const cookieOptions = {
+    expires: new Date(Date.now() + 10 * 1000),
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-  })
+    sameSite: sameSiteOption,
+  }
+
+  res.cookie("jwt", "", cookieOptions)
 
   res.status(200).json({
     status: "success",
